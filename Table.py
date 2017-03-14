9c40e06b95ae26a94c30cc15288986282a7c0093
Daryl <daryl.n.w.k@gmail.com>
2017-03-14 19:30:14 +0800

    Updated Table Class

--- Table.py
+++ Table.py
@@ -1,9 +1,11 @@
 from Column import *
+from WeakEntity import *
 class Table:
 	def __init__(self, tableId, name):
 		self.id = tableId			#table id as in xml file
 		self.name = name 			#table name as in xml file
 		self.columnList = [] 		#list of columns in table
+		self.weakEntityList = []
 
 	def getId(self):
 		return self.id
@@ -29,6 +31,12 @@ class Table:
 			if column.isInPrimaryKey():
 				primKey.append(column)
 		return primKey
+
+	def setWeakEntityList(self, weakEntityList):
+		self.weakEntityList = weakEntityList
+
+	def getWeakEntityList(self):
+		return self.weakEntityList
 		
 	def toXML(self):
 		return ""
