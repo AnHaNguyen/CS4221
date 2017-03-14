261b488871ddd830ec24f39f65aa7ebf5f525c29
Daryl <daryl.n.w.k@gmail.com>
2017-03-14 19:26:16 +0800

    Updated WeakEntity Class

--- WeakEntity.py
+++ WeakEntity.py
@@ -1,8 +1,19 @@
 from Type import *
 class WeakEntity:
-    def __init__(self, id):
-        self.id = id             #id as in xml file
-        self.datatype = Type.STRING     #type from Type enum class(let default be STRING)
+    def __init__(self, fromEntity, toEntity):
+        self.fromEntity = fromEntity
+        self.toEntity = toEntity
+        self.foreignKey = []
+        self.datatype = Type.STRING     # type from Type enum class(let default be STRING)
 
-    def getId(self):
-        return self.id; 
+    def getFromEntity(self):
+        return self.fromEntity;
+
+    def getToEntity(self):
+        return self.toEntity;
+
+    def setForeignKey(self, key):
+        self.foreignKey = key;
+
+    def getForeignKey(self):
+        return self.foreignKey;
