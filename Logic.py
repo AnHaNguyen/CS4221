6d1b1a534ca6ba3ced897554a2858dd8c34e04bb
WANG Hancheng <oriongump@gmail.com>
2017-03-17 23:11:36 +0800

    Add website source code. Modify the Logic.py so that command line argument can be parsed to specify different input file.

--- Logic.py
+++ Logic.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 from Table import *
 from ConstraintType import *
 from XMLSchema import *
+import argparse
 
 def processEntity(entityList):
     tableEntityList = []
@@ -128,75 +129,80 @@ def generateXMLSchema(tableEntityList, tableRelationshipList):
 	f.write(schema.toString())
 	f.close()
  
+if __name__ == '__main__':
+    # Environment Setting
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inFileName', help='A required string as input file name')
+    args = parser.parse_args()
 
-tree = ET.parse("template.xml")
-root = tree.getroot()
-entityList = []
-relationshipList = []
+    tree = ET.parse(args.inFileName)
+    root = tree.getroot()
+    entityList = []
+    relationshipList = []
 
-for child in root:
-    if (child.tag == "entity"):
-        entityList.append(child)
-    else:
-        relationshipList.append(child)
+    for child in root:
+        if (child.tag == "entity"):
+            entityList.append(child)
+        else:
+            relationshipList.append(child)
 
-primaryKeys = [[]] * len(entityList)
-dataTypes = [[]] * len(entityList)	#datatypes of primary keys
-weakEntityList = []
+    primaryKeys = [[]] * len(entityList)
+    dataTypes = [[]] * len(entityList)	#datatypes of primary keys
+    weakEntityList = []
 
-tableEntityList = processEntity(entityList)
-tableRelationshipList = processRelationship(relationshipList, tableEntityList)  
+    tableEntityList = processEntity(entityList)
+    tableRelationshipList = processRelationship(relationshipList, tableEntityList)  
 
-for table in tableEntityList:
-	# Save primary key as TABLE_NAME.COLUMN_NAME
-    keys = []
-    dataType = []
-    for key in table.getPrimaryKey():
-        keys.append(table.getName() + "." + key.getName())
-        dataType.append(key.getDataType())
+    for table in tableEntityList:
+    	# Save primary key as TABLE_NAME.COLUMN_NAME
+        keys = []
+        dataType = []
+        for key in table.getPrimaryKey():
+            keys.append(table.getName() + "." + key.getName())
+            dataType.append(key.getDataType())
 
-    primaryKeys[int(table.getId()) - 1] = keys
-    dataTypes[int(table.getId()) - 1] = dataType
+        primaryKeys[int(table.getId()) - 1] = keys
+        dataTypes[int(table.getId()) - 1] = dataType
 
-    weakEntityList.append(table.getWeakEntityList())
+        weakEntityList.append(table.getWeakEntityList())
 
-# Set weak entities foreign key(s)
-for weakEntities in weakEntityList:
-    for weakEntity in weakEntities:
-        weakEntity.setForeignKey(primaryKeys[int(weakEntity.getToEntity()) - 1])
-        weakEntity.setDataType(dataTypes[int(weakEntity.getToEntity()) - 1])
+    # Set weak entities foreign key(s)
+    for weakEntities in weakEntityList:
+        for weakEntity in weakEntities:
+            weakEntity.setForeignKey(primaryKeys[int(weakEntity.getToEntity()) - 1])
+            weakEntity.setDataType(dataTypes[int(weakEntity.getToEntity()) - 1])
 
-generateXMLSchema(tableEntityList, tableRelationshipList)
-
-
-#for debugging
-print(primaryKeys)
-print("\n--- Weak Entities ---")
-for weakEntities in weakEntityList:
-    for weakEntity in weakEntities:
-        print("Weak Entity: (From: " + weakEntity.getFromEntity() + ", To: " + weakEntity.getToEntity() + ")")
-        print("Foreign Key: " + str(weakEntity.getForeignKey()))
-
-print("--- End Weak Entities ---")
-
-for table in tableEntityList:
-    print("\nENTITY TABLE:"+ table.getId(), table.getName())    
-    columnList = table.getColumnList()
-    for column in columnList:
-        print("\nColumn:"+column.getId(), column.getName(), column.getDataType())
-        constraintList = column.getConstraintList()
-        for constraint in constraintList:
-            print(constraint.getConstraintType())
+    generateXMLSchema(tableEntityList, tableRelationshipList)
 
 
-for table in tableRelationshipList:
-    print("\nRELATIONSHIP TABLE:"+ table.getId(), table.getName())
-    columnList = table.getColumnList()
-    for column in columnList:
-        print("\nColumn:"+column.getId(), column.getName(), column.getDataType())
-        constraintList = column.getConstraintList()
-        for constraint in constraintList:
-            if (constraint.getConstraintType() == ConstraintType.PRIMARY_KEY):
+    #for debugging
+    print(primaryKeys)
+    print("\n--- Weak Entities ---")
+    for weakEntities in weakEntityList:
+        for weakEntity in weakEntities:
+            print("Weak Entity: (From: " + weakEntity.getFromEntity() + ", To: " + weakEntity.getToEntity() + ")")
+            print("Foreign Key: " + str(weakEntity.getForeignKey()))
+
+    print("--- End Weak Entities ---")
+
+    for table in tableEntityList:
+        print("\nENTITY TABLE:"+ table.getId(), table.getName())    
+        columnList = table.getColumnList()
+        for column in columnList:
+            print("\nColumn:"+column.getId(), column.getName(), column.getDataType())
+            constraintList = column.getConstraintList()
+            for constraint in constraintList:
                 print(constraint.getConstraintType())
-            else:
-                print(constraint.getConstraintType(), constraint.getData()[0], constraint.getData()[1])
+
+
+    for table in tableRelationshipList:
+        print("\nRELATIONSHIP TABLE:"+ table.getId(), table.getName())
+        columnList = table.getColumnList()
+        for column in columnList:
+            print("\nColumn:"+column.getId(), column.getName(), column.getDataType())
+            constraintList = column.getConstraintList()
+            for constraint in constraintList:
+                if (constraint.getConstraintType() == ConstraintType.PRIMARY_KEY):
+                    print(constraint.getConstraintType())
+                else:
+                    print(constraint.getConstraintType(), constraint.getData()[0], constraint.getData()[1])
